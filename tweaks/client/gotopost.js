df547ef788cb1a5f5ea5e20e7522e96dffda950c
alberto__segura <alberto.segura.delgado@gmail.com>
2015-08-21 11:41:03 +0200

    remove go to reply, now it has been added to exodo composer

--- tweaks/client/gotopost.js
+++ tweaks/client/gotopost.js
@@ -4,15 +4,16 @@
 (function () {
 	"use strict";
 
+	/*
 	$(window).on('action:composer.posts.reply', function (event, data) {
 		
 		var topicUrl = "/topic/" + ajaxify.variables.get('topic_slug') + "/" + (parseInt(ajaxify.variables.get('postcount'))+1);
 		//document.location.href = window.location.protocol + "//" + window.location.host + "/topic/" + topicUrl;
 		//ajaxify.go(topicUrl);
 
-		/*
-		Ante una nueva respuesta, comprobamos si estamos en la última página. 
-		 * Si es así, hacemos scroll hasta el último post. Si no, esperamos al cambio automático de página */
+		
+		// Ante una nueva respuesta, comprobamos si estamos en la última página. 
+		// Si es así, hacemos scroll hasta el último post. Si no, esperamos al cambio automático de página
 		if ( ajaxify.variables.get('currentPage') === ajaxify.variables.get('pageCount') ) {
 			scrollToBottom();
 		} else {
@@ -27,5 +28,6 @@
 			}, 500)
 		}, 500);
 	}
+	*/
 
 }());
